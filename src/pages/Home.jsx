79fb20f9dd7d3cf656ce612fb68ca6dda87984fb
Drake Sandacz <drakeeman@yahoo.com>
2025-11-21 09:52:00 -0600

    First Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      backgroundColor: "black", 
+      color: "white", 
+      height: "100vh",
+      padding: "2rem"
+    }}>
+      <h1>HOME IS WORKING 🎉</h1>
+      <p>If you see this, routing is fine.</p>
+    </div>
+  );
+}
